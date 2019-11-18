a525d200ce78159ab760d6617059d0e262411ee2
LAHdz <leonidas0197@gmail.com>
2019-11-18 04:15:23 -0600

    version 1

---
+++ src/huellahidrica/HuellaHidrica.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package huellahidrica;
+
+import huellahidrica.View.PrincipalUI;
+
+/**
+ *
+ * @author asael
+ */
+public class HuellaHidrica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        PrincipalUI ui = new PrincipalUI();
+        ui.setResizable(false);
+        ui.setLocationRelativeTo(null);
+        ui.setVisible(true);
+    }
+    
+}
